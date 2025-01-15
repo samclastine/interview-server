a2a85ad69bec78a23f4357d57488f96571345a72
Sam Clastine <62068586+samclastine@users.noreply.github.com>
2025-01-15 10:45:37 +0000

    new

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: '*', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
-  })
-);
 
 app.use('/api/payload', payloadRouter);
 
